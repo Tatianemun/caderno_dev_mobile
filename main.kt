131c1fe9fbfb06d1bcaf930c71f5a883bad86120
Tatianemun <160507980+Tatianemun@users.noreply.github.com>
2024-09-05 09:30:19 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,12 @@
+fun main() {
+  /* 
+  Programa teste de funções em Kotlin
+  Programador: Tatiane Muniz
+  */
+  println("Olá mundo")
+  //println(5+6)
+  println(3*4)
+  var frase="Uma frase do tipo string"
+  frase="mudei a frase"
+  println(frase) 
+}
