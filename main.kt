2dfc7329d28e4bbab476549d189a841950f5d009
Tatianemun <160507980+Tatianemun@users.noreply.github.com>
2024-10-16 11:42:37 -0300

    Add files via upload

--- main.kt
+++ main.kt
@@ -1,12 +1,17 @@
 fun main() {
-  /* 
-  Programa teste de funções em Kotlin
-  Programador: Tatiane Muniz
-  */
-  println("Olá mundo")
-  //println(5+6)
-  println(3*4)
-  var frase="Uma frase do tipo string"
-  frase="mudei a frase"
-  println(frase) 
+  println("Digite um número inteiro:")
+  var num1:Int=readLine()?.toIntOrNull()?:0
+  println("Digite outro número inteiro:")
+  var num2:Int=readLine()?.toIntOrNull()?:0
+  var result=num1+num2
+  println("A soma é: $result")
+
+  var sub:Int=num1-num2
+  println("A subtração é: $sub")
+
+  var mult:Int= num1*num2
+  println("A multiplicação é: $mult")
+
+  var div:Int= num1/num2
+  println("A divisão é: $div")
 }
